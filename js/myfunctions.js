8a8057192f33ea1c41b69a06ae74024653e096b5
angelRep <angel_ang_elina@live.com>
2022-10-19 16:38:39 +0300

    Update contact form

--- js/myfunctions.js
+++ js/myfunctions.js
@@ -1,1 +1,16 @@
 /*Functions*/
+function sendContactForm() {
+    fname = document.getElementById("fullname")
+    email = document.getElementById("email")
+    subject = document.getElementById("subject")
+    message = document.getElementById("message")
+
+    window.alert("Thank you for you message!")
+    delete window.alert; // true
+    alert('test');
+
+    fname.innerText = ""
+    email.innerText = ""
+    subject.innerText = ""
+    message.innerText = ""
+}
